9b058abdc17a861aec58fc59b02ee861bf23cd1f
nathan <nathanbatista7312@gmail.com>
2023-09-21 21:20:17 -0300

    Initial commit

---
+++ src/exercicio_nota_escolar.java
@@ -0,0 +1,16 @@
+public class exercicio_nota_escolar {
+
+    public static void main(String[] args){
+
+        double nota1 = 3;
+        double nota2 = 9;
+        double PESO_NOTA = 0.5;
+        double media = (nota1 * PESO_NOTA) + (nota2 * PESO_NOTA);
+
+        System.out.println("Nota 1: " + nota1);
+        System.out.println("Nota 2: " + nota2);
+        System.out.println("Média: " + media);
+
+
+    }
+}
