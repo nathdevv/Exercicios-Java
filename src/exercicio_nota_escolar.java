8868889aa1e9f8f6c610eaa18300d9d7e4a85050
nathan <nathanbatista7312@gmail.com>
2023-09-23 00:07:15 -0300

    Atualização

--- src/exercicio_nota_escolar.java
+++ src/exercicio_nota_escolar.java
@@ -2,14 +2,16 @@ public class exercicio_nota_escolar {
 
     public static void main(String[] args){
 
-        double nota1 = 3;
-        double nota2 = 9;
-        double PESO_NOTA = 0.5;
-        double media = (nota1 * PESO_NOTA) + (nota2 * PESO_NOTA);
+        // Podemos colocar as notas e medias em uma só variável porque todas foram instituídas como double
+        double nota1, nota2, media;
+        // "final double" é utilizado para definir quando uma variável é constante
+        final double PESO_NOTA = 0.5;
+        nota1 = 8;
+        nota2 = 6.5;
+        media = (nota1 * PESO_NOTA) + (nota2 * PESO_NOTA);
 
-        System.out.println("Nota 1: " + nota1);
-        System.out.println("Nota 2: " + nota2);
-        System.out.println("Média: " + media);
+
+        System.out.println("A Média é: " + media);
 
 
     }
