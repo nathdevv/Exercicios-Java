874acc74fbddd62ef8ead3bec790e9da7ee8aa2c
nathan <nathanbatista7312@gmail.com>
2023-09-23 23:56:36 -0300

    Exercícios

--- src/Média_Nota_Escola.java
+++ src/Média_Nota_Escola.java
@@ -2,9 +2,7 @@ public class Média_Nota_Escola {
 
     public static void main(String[] args){
 
-        // Podemos colocar as notas e medias em uma só variável porque todas foram instituídas como double.
         double nota1, nota2, media;
-        // "final double" é utilizado para definir quando uma variável é constante.
         final double PESO_NOTA = 0.5;
         nota1 = 8;
         nota2 = 6.5;
